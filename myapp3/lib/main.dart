bad2a87299e1607ab91c5961805632a2ab45f8e3
LatifovMuhammadjon <latifov7864@gmail.com>
2022-06-23 17:12:02 +0500

    file is added

---
+++ myapp3/lib/main.dart
@@ -0,0 +1,135 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.indigo,
+        fontFamily: "Pacifico",
+      ),
+      color: Colors.indigo,
+      home: const HomeWork(),
+    ),
+  );
+}
+
+class HomeWork extends StatefulWidget {
+  const HomeWork({Key? key}) : super(key: key);
+
+  @override
+  State<HomeWork> createState() => _HomeWorkState();
+}
+
+class _HomeWorkState extends State<HomeWork> {
+  late int _counter;
+  @override
+  void initState() {
+    super.initState();
+    _counter = 0;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.indigo[300],
+      appBar: AppBar(
+        backgroundColor: Colors.indigo,
+        title: const Text("HomeWork"),
+        centerTitle: true,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          const Padding(
+            padding: EdgeInsets.all(30),
+            child: Text(
+              'Tap "-" to decrement',
+              style: TextStyle(
+                fontSize: 28,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              MaterialButton(
+                height: 60,
+                color: Colors.indigo[400],
+                child: const Icon(
+                  Icons.remove,
+                  size: 32,
+                  color: Colors.white,
+                ),
+                onPressed: () {
+                  setState(() {
+                    _counter=_counter>0?_counter-1:0;
+                  });
+                },
+              ),
+              MaterialButton(
+                height: 60,
+                color: Colors.indigo[400],
+                child: Text(
+                  "$_counter",
+                  style: const TextStyle(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                onPressed: () {},
+              ),
+              MaterialButton(
+                height: 60,
+                color: Colors.indigo[400],
+                child: const Icon(
+                  Icons.add,
+                  size: 32,
+                  color: Colors.white,
+                ),
+                onPressed: () {
+                  setState(() {
+                    _counter++;
+                  });
+                },
+              ),
+            ],
+          ),
+          const Padding(
+            padding: EdgeInsets.all(30),
+            child: Text(
+              'Tap "+" to increment',
+              style: TextStyle(
+                fontSize: 28,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+// class LessonApp extends StatelessWidget {
+//   const LessonApp({Key? key}) : super(key: key);
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text("Lesson App"),
+//       ),
+//       body: const Center(
+//         child: Text(
+//           "Hello World",
+//           style: TextStyle(
+//             fontSize: 28,
+//             color: Colors.indigo,
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
